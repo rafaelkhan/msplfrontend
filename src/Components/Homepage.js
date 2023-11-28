4840ba48bdaf308b9dada66a77735fa5ce8d25e8
rafaelkhan <rafaelkhan06@gmail.com>
2023-11-28 14:04:48 +0100

    Hase

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import './Homepage.css';
 
-const Homepage = () => {
+function Homepage(){
     return (
         <div className="homepage-container">
             <h1><span className="mspl-text">MSPL</span> - Willkommen auf unserer Website</h1>
@@ -15,6 +15,6 @@ const Homepage = () => {
             </a>
         </div>
     );
-};
+}
 
 export default Homepage;
