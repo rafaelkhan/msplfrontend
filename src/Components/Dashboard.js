1e653adf28db828c9b9a332f298c6a286638313d
sufisameer <sufisameer136@gmail.com>
2023-12-05 16:54:43 +0100

    login files

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,15 +1,33 @@
 // Dashboard.js
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import './Dashboard.css';
+import { signOut } from './authProvider';
 
 function Dashboard() {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await signOut();
+            navigate('/Homepage');
+        } catch (error) {
+            console.error('An error occurred during logout:', error);
+        }
+    };
+
     return (
         <div className="dashboard">
             <Sidebar />
             <div className="dashboard-content">
                 <h1>Dashboard Content</h1>
+
+                {/* Hier ist der Logout-Button */}
+                <button className="logout-button" onClick={handleLogout}>
+                    Logout
+                </button>
             </div>
         </div>
     );
