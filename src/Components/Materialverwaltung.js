0c33d4d1690c01541b465fee74903a15bb875fe2
MathiasMuth <muthmathias656@gmail.com>
2023-12-07 19:57:58 +0100

    hintergrundfarbe

--- src/Components/Materialverwaltung.js
+++ src/Components/Materialverwaltung.js
@@ -9,7 +9,6 @@ import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { Box, Badge, Button, ButtonGroup, TextField } from '@mui/material';
 
-
 function Materialverwaltung() {
     const [count, setCount] = React.useState(1);
     const [invisible, setInvisible] = React.useState(false);
@@ -19,65 +18,67 @@ function Materialverwaltung() {
     };
 
     return (
-        <div style={{ display: 'flex' }}>
-            <Sidebar />
-            <div className="dashboard-content">
-                <h1>Materialverwaltung</h1>
-                <div className="Search-ID">
+        <div className="body">
+            <div style={{ display: 'flex' }}>
+                <Sidebar />
+                <div className="dashboard-content">
+                    <h1>Materialverwaltung</h1>
+                    <div className="Search-ID">
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField
+                                id="filled-multiline-flexible"
+                                label="Search ID"
+                                multiline
+                                maxRows={4}
+                                variant="filled"
+                            />
+                        </Box>
+                    </div>
                     <Box
-                        component="form"
                         sx={{
-                            '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            color: 'action.active',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            '& > *': {
+                                marginBottom: 2,
+                            },
+                            '& .MuiBadge-root': {
+                                marginRight: 4,
+                            },
                         }}
-                        noValidate
-                        autoComplete="off"
                     >
-                        <TextField
-                            id="filled-multiline-flexible"
-                            label="Search ID"
-                            multiline
-                            maxRows={4}
-                            variant="filled"
-                        />
+                        <div className="edit">
+                            <Badge color="secondary" badgeContent={count}>
+                                <LocalGroceryStoreIcon style={{ color: 'black' }} />
+                            </Badge>
+                            <ButtonGroup>
+                                <Button
+                                    aria-label="reduce"
+                                    onClick={() => {
+                                        setCount(count - 1);
+                                    }}
+                                >
+                                    <RemoveIcon fontSize="small" style={{ color: 'black' }} />
+                                </Button>
+                                <Button
+                                    aria-label="increase"
+                                    onClick={() => {
+                                        setCount(count + 1);
+                                    }}
+                                >
+                                    <AddIcon fontSize="small" style={{ color: 'black' }} />
+                                </Button>
+                            </ButtonGroup>
+                        </div>
                     </Box>
                 </div>
-                <Box
-                    sx={{
-                        color: 'action.active',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        '& > *': {
-                            marginBottom: 2,
-                        },
-                        '& .MuiBadge-root': {
-                            marginRight: 4,
-                        },
-                    }}
-                >
-                    <div className="edit">
-                        <Badge color="secondary" badgeContent={count}>
-                            <LocalGroceryStoreIcon/>
-                        </Badge>
-                        <ButtonGroup>
-                            <Button
-                                aria-label="reduce"
-                                onClick={() => {
-                                    setCount(Math.max(count - 1, 0));
-                                }}
-                            >
-                                <RemoveIcon fontSize="small" />
-                            </Button>
-                            <Button
-                                aria-label="increase"
-                                onClick={() => {
-                                    setCount(count + 1);
-                                }}
-                            >
-                                <AddIcon fontSize="small" />
-                            </Button>
-                        </ButtonGroup>
-                    </div>
-                </Box>
             </div>
         </div>
     );
