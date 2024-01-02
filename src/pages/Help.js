e6f1d69235bd906103c3d19d0f9d01acb3e814ed
MathiasMuth <muthmathias656@gmail.com>
2024-01-02 17:58:37 +0100

    auf der seite help ein FAQ eingefuegt

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import Sidebar from '../Components/Sidebar';
-import '../CSS/General.css';
+import Faq from "react-faq-component";
 
 function Help() {
+
+    const data = {
+        title: "FAQ",
+        rows: [
+            {
+                title: "Erklärung der Webaplikation.",
+                content: `Die Website ist mit einer Datenbank im backend verbunden. In der Datenbank sind Materialien des SPL 
+                des SZU gespeichert. Wenn Sie ein Material entnehmen, dann kennzeichnen Sie dies bitte in der Materialverwaltung.`,
+            },
+            {
+                title: "Wie ist die Webaplikation anzuwenden.",
+                content: `Sie melden sich mit Ihrem Schul-Microsoft Account ein und gehen zur Materialübersicht. Dort suchen 
+                Sie dann die ID oder den Namen des Materiales, welches Sie genommen haben und kennzeichnen das mit einem 
+                klick auf das "-".`,
+            },
+            {
+                title: "Was mache ich, wenn ich vergessen habe ein Produkt, welches ich genommen habe einzutragen.",
+                content: `Besprechen Sie näheres mit der Lehrperson. Im Notfall tragen Sie es einfach im nachhinein ein. `,
+            },
+        ],
+    };
+
+    const styles = {
+        titleTextColor: "black",
+        rowTitleColor: "black",
+        rowContentColor: 'grey',
+
+    };
+
+    const config = {
+        animate: true,
+        arrowIcon: "^",
+        tabFocus: true
+    };
+
     return (
-        <div style={{ display: 'flex' }}>
-            <Sidebar/>
+        <div style={{ display: 'flex', justifyContent: 'center' }}> {/* Fan-Bereich in die Mitte verschieben */}
+            <Sidebar />
             <div className="content">
                 <h1 className="Titel">Help</h1>
+                <div className="content" style={{ textAlign: 'center' }}>
+                <Faq
+                    data={data}
+                    styles={styles}
+                    config={config}
+                />
+                </div>
             </div>
         </div>
     );
