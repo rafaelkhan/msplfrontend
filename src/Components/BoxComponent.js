beb1a75600bc822099019fa4b8821e8c80909ee5
MathiasMuth <muthmathias656@gmail.com>
2024-01-29 21:37:41 +0100

    onhover farbenänderung bei materialansicht

--- src/Components/BoxComponent.js
+++ src/Components/BoxComponent.js
@@ -4,55 +4,54 @@ import { Link } from 'react-router-dom';
 import '../CSS/BoxComponent.css';
 
 const BoxComponent = ({ box }) => {
-    const [hover, setHover] = useState(false);
-    const [overlayVisible, setOverlayVisible] = useState(false);
-    const isActive = box.Menge > 0;
-    const isEmpty = box.Bezeichnung != null;
+    const [isHovered, setIsHovered] = useState(false);
 
-    const handleMouseMove = (event) => {
-        setHover(true);
-        setOverlayVisible(true);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
     };
 
     const handleMouseLeave = () => {
-        setHover(false);
-        setOverlayVisible(false);
+        setIsHovered(false);
     };
 
+    const isActive = box.Menge > 0;
+    const isEmpty = box.Bezeichnung != null;
+
     return (
-        <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-            <Card
-                className="boxContainer"
-                raised
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    height: '4vw',
-                    alignItems: 'center',
-                    backgroundColor: isActive ? '#A7C7E7' : (isEmpty ? '#ff6961' : '#cfcfc4'),
-                    position: 'relative',
-                    transition: 'box-shadow 0.3s',
-                }}
-            >
-                <Typography variant="caption" sx={{ position: 'absolute', top: '5px', right: '5px' }}>
-                    Box {box.BoxID}
-                </Typography>
-                {(isActive || isEmpty) && hover && (
-                    <Tooltip
-                        title={
-                            <React.Fragment>
-                                <Typography color="inherit">{box.Bezeichnung}</Typography>
-                                {"Menge: " + box.Menge}
-                            </React.Fragment>
-                        }
-                        open={overlayVisible}
-                        placement="top"
-                    >
-                        <Link to={`/material-detail/${box.BoxID}`} style={{ textDecoration: 'none', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%', zIndex: 1000 }} />
-                    </Tooltip>
-                )}
-            </Card>
-        </div>
+        <Card
+            className="boxContainer"
+            raised
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                height: '4vw',
+                alignItems: 'center',
+                backgroundColor: isHovered ? '#A9A9A9n' : (isActive ? '#A7C7E7' : (isEmpty ? '#ff6961' : '#cfcfc4')),
+                position: 'relative',
+                transition: 'background-color 0.3s',
+            }}
+        >
+            <Typography variant="caption" sx={{ position: 'absolute', top: '5px', right: '5px' }}>
+                Box {box.BoxID}
+            </Typography>
+
+            {(isActive || isEmpty) && (
+                <Tooltip
+                    title={
+                        <React.Fragment>
+                            <Typography color="inherit">{box.Bezeichnung}</Typography>
+                            {"Menge: " + box.Menge}
+                        </React.Fragment>
+                    }
+                    open={isHovered}
+                    placement="top"
+                >
+                    <Link to={`/material-detail/${box.BoxID}`} style={{ textDecoration: 'none', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%', zIndex: 1000 }} />
+                </Tooltip>
+            )}
+        </Card>
     );
 };
 
